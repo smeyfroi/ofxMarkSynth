8274eaaf89212f5c905020890042fabee3e536da
smeyfroi <steve.meyfroidt@gmail.com>
2025-09-20 20:34:40 +0100

    Use OpenGL 4.1

--- example_simple/src/main.cpp
+++ example_simple/src/main.cpp
@@ -1,6 +1,13 @@
 #include "ofApp.h"
+#include "ofMain.h"
 
 int main(){
-	ofSetupOpenGL(768, 768, OF_WINDOW);
-	ofRunApp(new ofApp());
+  // use GLFW window settings with GL 4.1
+  ofGLFWWindowSettings settings;
+  settings.setGLVersion(4, 1);
+  settings.setSize(768, 768);
+  settings.windowMode = OF_WINDOW;
+  ofCreateWindow(settings);
+  
+  ofRunApp(new ofApp());
 }
