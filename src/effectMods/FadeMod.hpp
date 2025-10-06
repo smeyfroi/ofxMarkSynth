7fa216158d0a94f7610fd7e5282457818bb5ae55
smeyfroi <steve.meyfroidt@gmail.com>
2025-10-06 15:35:26 +0100

    Increase fade param range

--- src/effectMods/FadeMod.hpp
+++ src/effectMods/FadeMod.hpp
@@ -32,7 +32,7 @@ protected:
 private:
 //  ofParameter<glm::vec2> translationParameter { "Translation", glm::vec2 { 0.0, 0.0 }, glm::vec2 { -0.01, -0.01 }, glm::vec2 { 0.01, 0.01 } };
 //  ofParameter<float> alphaParameter { "Translation Alpha", 1.0, 0.99, 1.0 };
-  ofParameter<float> fadeAmountParameter { "Fade Amount", 0.0001, 0.0, 0.01 };
+  ofParameter<float> fadeAmountParameter { "Fade Amount", 0.0001, 0.0, 0.05 };
 
 //  TranslateEffect translateEffect;
   FadeEffect fadeEffect;
